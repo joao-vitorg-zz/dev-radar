b7ee98640d3d22b58a40bb52c74e0e57cde7f435
oDallas <jovitor737@gmail.com>
2020-01-25 01:54:01 -0300

    Introdução do SocketIO

--- backend/src/websocket.js
+++ backend/src/websocket.js
@@ -1,9 +1,38 @@
 const socketio = require('socket.io');
+const parseStringAsArray = require('./utils/parseStringAsArray');
+const calculateDistance = require('./utils/calculateDistance');
+
+let io;
+const connections = [];
 
 exports.setupWebsocket = server => {
-	const io = socketio(server);
+	io = socketio(server);
 
 	io.on('connection', socket => {
-		console.log(socket.io);
+		const { latitude, longitude, techs } = socket.handshake.query;
+
+		connections.push({
+			id: socket.id,
+			coordinates: {
+				latitude: Number(latitude),
+				longitude: Number(longitude)
+			},
+			techs: parseStringAsArray(techs)
+		});
 	});
+};
+
+exports.findConnectionsAndSendMessage = (dev, message) => {
+	const { location, techs } = dev;
+
+	connections
+		.filter(connection => {
+			return (
+				calculateDistance(location.coordinates, connection.coordinates) < 10 &&
+				connection.techs.some(item => techs.includes(item))
+			);
+		})
+		.forEach(connection => {
+			io.to(connection.id).emit(message, dev);
+		});
 };
